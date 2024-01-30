a8ae7639751dee984a3036cc0458f51b6882e67f
Derrick K <67762448+DerrickKJr@users.noreply.github.com>
2024-01-30 16:02:08 -0500

    Image loading

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function loadFarm(){
   const myPlots = document.getElementsByTagName("td");
   for (var x = 0; x < myPlots.length; x++) {
     console.log("Plot: " + myPlots[x].innerHTML + " has loaded.");
-    //myPlots[x].innerHTML = farm[x].plant;
+    myPlots[x].children[0].src = "sprites/" + farm[x].plant + ".png";
   }
 }
 function plotClick(id){
@@ -29,6 +29,7 @@ var holding1 = false;
 var holding2 = false;
 var heldCrop;
 document.addEventListener("mousemove", function(e){
+  document.getElementById("moDisplay").innerHTML = "Current mo: " + mo;
   mm = true;
   if(md == true && mm == true){
     //console.log("Moving with mouse down!");
@@ -37,8 +38,9 @@ document.addEventListener("mousemove", function(e){
     if(!holding1){
       if(!holding2){
         heldCrop = document.createElement("img");
-        heldCrop.src = "sprites/Pumpkin.png";//CHANGE
+        heldCrop.src = "sprites/" + farm[mo].plant + ".png";//"sprites/Pumpkin.png";//CHANGE
         document.getElementById('body').appendChild(heldCrop);
+        heldCrop.style.pointerEvents = "none";
         heldCrop.style.position = "absolute";
         heldCrop.style.transform = "scale(2)";
         heldCrop.id = "heldItem";
@@ -47,7 +49,7 @@ document.addEventListener("mousemove", function(e){
       heldCrop.style.left = e.pageX-10 + "px";
       heldCrop.style.top = e.pageY-10 + "px";
     }
-    console.log(e.pageX, e.pageY);
+    // console.log(e.pageX, e.pageY);
   }
 });
 document.addEventListener("mouseup", function(event){
@@ -63,4 +65,12 @@ document.addEventListener("mouseup", function(event){
 });
 
 farm[0].plant = "Pumpkin";
+farm[1].plant = "Eggplant";
+farm[2].plant = "Cauliflower";
+farm[3].plant = "Carrot";
+farm[4].plant = "Potato";
+farm[5].plant = "Tomato";
+farm[6].plant = "Watermelon";
+farm[7].plant = "Corn";
+farm[8].plant = "Cabbage";
 console.log(farm);
